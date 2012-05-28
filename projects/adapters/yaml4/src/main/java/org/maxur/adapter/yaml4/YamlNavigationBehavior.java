8e556bdc1ff305829448cb6b58c82f58c1a70e60
Maxim Yunusov <maximyunusov@gmail.com>
2012-05-28 10:21:15 +0700

    DEV: Migrate to YAML 4 Refactoring

--- projects/adapters/yaml4/src/main/java/org/maxur/adapter/yaml4/YamlNavigationBehavior.java
+++ projects/adapters/yaml4/src/main/java/org/maxur/adapter/yaml4/YamlNavigationBehavior.java
@@ -1,12 +1,10 @@
 package org.maxur.adapter.yaml4;
 
-import com.google.inject.Inject;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.maxur.commons.component.behavior.BaseResourcesBehavior;
-import org.maxur.commons.component.model.webclient.WebBrowserDetector;
 import org.maxur.commons.view.api.StyleBehavior;
 
 /**
@@ -21,9 +19,6 @@ public class YamlNavigationBehavior extends BaseResourcesBehavior implements Sty
      * The Serial Version UID.
      */
     private static final long serialVersionUID = -1362412483333201738L;
-
-    @Inject
-    private WebBrowserDetector detector;
 
     @Override
     public void renderHead(Component component, IHeaderResponse response) {
